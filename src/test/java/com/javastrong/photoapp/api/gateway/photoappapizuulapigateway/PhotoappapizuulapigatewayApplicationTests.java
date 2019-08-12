942ef426d731f769cdc2ba10cf5ae7d65ca4e48c
Ken Gardiner <kagemail@gmail.com>
2019-08-12 07:52:50 -0400

    Initial commit

---
+++ src/test/java/com/javastrong/photoapp/api/gateway/photoappapizuulapigateway/PhotoappapizuulapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javastrong.photoapp.api.gateway.photoappapizuulapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappapizuulapigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
